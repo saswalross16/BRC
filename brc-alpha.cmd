cff453bc17d4dff0837d72043758b9003eaf194d
saswalross16 <39779630+saswalross16@users.noreply.github.com>
2018-05-30 23:01:40 +0200

    Add files via upload

---
+++ brc-alpha.cmd
@@ -0,0 +1,146 @@
+@echo off
+title BRC v. 1.0 [ALPHA]
+echo Bing Point-Catcher Version 1.0 [ALPHA]
+echo.
+echo Um abzubrechen, schliesse das Fenster! Um Fortzufahren, druecke eine beliebige Taste!
+pause
+cls
+echo Druecke erneut, um zu bestaetigen!
+pause
+cls
+timeout /T 10 /nobreak
+cls
+goto a
+:a
+start https://www.bing.com/search?q=1&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=2&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=3&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=3&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=4&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=5&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=6&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=7&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=8&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=9&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=10&go=Suche&qs=ds&form=QBRE
+cls
+timeout /T 10 /nobreak
+taskkill /F /IM firefox.exe /T
+cls
+goto b
+:b
+start https://www.bing.com/search?q=Stephen+Hawking&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Angela+Merkel&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Cupcakes&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Fußball&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=American+Football&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Deutschland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Österreich&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Schweiz&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Schweden&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Norwegen&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Finnland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Dänemark&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Polen&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Frankreich&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Liechtenstein&go=Suche&qs=ds&form=QBRE
+cls
+timeout /T 10 /nobreak
+taskkill /F /IM firefox.exe /T
+cls
+goto c
+:c
+start https://www.bing.com/search?q=Italien&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Vereinigtes+Königeich&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Irland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Tschechische+Republik&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Rumänien&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Weißrussland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Estland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Lettland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Kroatien&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Türkei&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Spanien&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Portugal&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Russland&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Kasachstan&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=China&go=Suche&qs=ds&form=QBRE
+cls
+timeout /T 10 /nobreak
+taskkill /F /IM firefox.exe /T
+cls
+goto d
+:d
+start https://www.bing.com/search?q=Japan&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=USA&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Indien&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Australien&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Libelle&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Blauwal&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Gorilla&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Frosch&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Krabbe&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Apfel&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Banane&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Avocado&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Suppe&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Vetilator&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Notebook&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Glas&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Wasser&go=Suche&qs=ds&form=QBRE
+cls
+timeout /T 10 /nobreak
+taskkill /F /IM firefox.exe /T
+cls
+goto e
+:e
+start https://www.bing.com/search?q=Feuer&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Erde&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Weltraum&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Astronaut&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Planet+Mars&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Planet+Saturn&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=Jupiter&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+cls
+timeout /T 10 /nobreak
+taskkill /F /IM firefox.exe /T
+cls
+goto f
+:f
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+cls
+timeout /T 10 /nobreak
+taskkill /F /IM firefox.exe /T
+cls
+goto g
+:g
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+start https://www.bing.com/search?q=&go=Suche&qs=ds&form=QBRE
+cls
+taskkill /F /IM firefox.exe /T
+cls
+echo Die Punkte wurden gesammelt und das Programm wird geschlossen.
+pause
+exit
